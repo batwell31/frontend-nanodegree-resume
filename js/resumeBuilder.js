8946d169f7fe39f85f8a0037468a8966ea74b78b
Brandon Lothaire Atwell <batwell31@yahoo.com>
2018-05-27 12:00:12 -0400

    add empty shell to objects in resumeBuilder.js

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,44 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+var bio = {
+    name: string
+    role: string
+    contacts: an object with
+          mobile: string
+          email: string 
+          github: string
+          twitter: string(optional)
+          location: string
+    welcomeMessage: string 
+    skills: array of strings
+    biopic: string url
+    display: function taking no parameters
+};
+
+var education = {
+    schools: array of objects with
+         name: string
+         location: string
+         degree: string
+         majors: array of strings
+         dates: string(works with a hyphen between them)
+    url: string(optional)
+    onlineCourses: array of objects with
+         title: string
+         school: string
+         dates: string(works with a hyphen between them)
+    url: string
+    display: function taking no parameters
+
+};
+
+var work = {
+    projects: array of objects with
+          title: string 
+          dates: string(works with a hyphen between them)
+    description: string
+          images: array with string urls
+    display: function taking no parameters
+};
+
+var projects = {
+
+};
